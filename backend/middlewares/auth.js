156e524c1da4f01851693b51823236d194138c48
Kristina Epifanova <cristinaepifanova@gmail.com>
2023-02-23 20:48:40 +0300

    fixed errors with env file

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/401-UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET = 'a56e4e2f235ea7966935a4c53ae8b99b' } = process.env;
-
+const { NODE_ENV, JWT_SECRET } = process.env;
+const JWT_SECRET_DEV = 'a56e4e2f235ea7966935a4c53ae8b99b';
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV);
   } catch (err) {
     return next(new UnauthorizedError('Ошибка верификации токена'));
   }
